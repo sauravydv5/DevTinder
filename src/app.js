ec82f652ab61fa9c07961205839e02215d4aefa0
saurav kumar <sauravkumar91937@gmail.com>
2025-05-17 12:48:05 +0530

    errorHandling

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
 const app = express();
-const { adminAuth, userAuth } = require("./middleware/auth");
+// const { adminAuth, userAuth } = require("./middleware/auth");
 
-app.use("/admin", adminAuth);
+// app.use("/admin", adminAuth);
 // app.use("/user", userAuth);
 
 // app.post("./user/login", (req, res) => {
@@ -11,19 +11,41 @@ app.use("/admin", adminAuth);
 // });
 
 //For admin auth
-app.get("/admin/getAlldata", (req, res) => {
-  res.send("All Data Sent.....");
-});
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("All user Delete.....");
-});
-app.get("/admin/patchUser", (req, res) => {
-  res.send("All user Patched.....");
+// app.get("/admin/getAlldata", (req, res) => {
+//   res.send("All Data Sent.....");
+// });
+// app.get("/admin/deleteUser", (req, res) => {
+//   res.send("All user Delete.....");
+// });
+// app.get("/admin/patchUser", (req, res) => {
+//   res.send("All user Patched.....");
+// });
+
+// //for userauth
+// app.get("/user/getAlldata", userAuth, (req, res) => {
+//   res.send("All Data Sent.....");
+// });
+
+app.get("/getUserData", (req, res) => {
+  //logic of db call and get user data
+
+  // try{
+
+  // }catch(err){
+
+  // }
+
+  throw new Error("jdadhad");
+  res.send("User Data sent...");
 });
 
-//for userauth
-app.get("/user/getAlldata", userAuth, (req, res) => {
-  res.send("All Data Sent.....");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong");
+  }
+  //  else {
+  //   next();
+  // }
 });
 
 app.listen(3000, () => {
